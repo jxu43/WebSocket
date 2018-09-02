9467332d37f2514025f96e8a110439d3e0f149a9
Jiahao Xu <jiahao-xu@Jiahaos-MBP.fios-router.home>
2018-09-02 14:10:29 -0400

    websocket base

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App set up
+var app = express();
+var server = app.listen(4000, function() {
+    console.log('listen to port 4000')
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket Setup
+var io = socket(server);
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+});
